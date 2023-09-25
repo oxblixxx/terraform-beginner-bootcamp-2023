3b5b4e8a0c7a72c957645d331561a75bfe81f280
Mutapha Pelumi Babatunde <phamust1111@gmail.com>
2023-09-25 13:16:29 +0000

    #8 Deploy a S3 bucket with random string

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+     aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
 
@@ -16,9 +20,25 @@ resource "random_string" "bucket-name" {
   length = 8
   special = false
   override_special = ""
+  lower = false
+  upper = false
 }
 
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "bootcamp-bucket" {
+  bucket = random_string.bucket-name.result
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
 output "bucket-name" {
   value = random_string.bucket-name.result
+}
+
+output "bootcamp-bucket" {
+  value = aws_s3_bucket.bootcamp-bucket.bucket
 }
