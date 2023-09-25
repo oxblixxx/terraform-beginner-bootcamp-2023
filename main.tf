be11e2d21b9c9d11f9c1275b40eb69e610b49414
Mutapha Pelumi Babatunde <97739416+oxblixxx@users.noreply.github.com>
2023-09-25 06:32:06 +0100

    6 create a random terraform bucket name (#7)
     #6 Generate a random bucket name

--- main.tf
+++ main.tf
@@ -1,1 +1,24 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
 
+provider "random" {
+  # Configuration options
+}
+
+
+resource "random_string" "bucket-name" {
+  length = 8
+  special = false
+  override_special = ""
+}
+
+
+output "bucket-name" {
+  value = random_string.bucket-name.result
+}
