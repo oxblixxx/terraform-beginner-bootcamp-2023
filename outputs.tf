4066ff97216ca442f179e3a5259f9dae0a9a73b5
Mutapha Pelumi Babatunde <phamust1111@gmail.com>
2023-09-27 19:06:43 +0000

    #21 fixed undeclared errors.

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "bucket-name" {
-  value = random_string.bucket-name.result
-}
-
 output "bootcamp-bucket" {
   value = aws_s3_bucket.bootcamp-bucket.bucket
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "name" {
-    type = string
-    description = "AWS Terraform Bootcamp 2023"
+  type        = string
+  description = "AWS Terraform Bootcamp 2023"
 }
 
 variable "environment" {
-    type = string
+  type = string
 }
 
 variable "user-uuid" {
-    description = "Exampro user-uuid"
-# https://developer.hashicorp.com/terraform/language/expressions/custom-conditions#input-variable-validation    
-     validate {
-    condition = length(var.bucket) >= 3 && length(var.bucket) <= 63
+  description = "Exampro user-uuid"
+  # https://developer.hashicorp.com/terraform/language/expressions/custom-conditions#input-variable-validation    
+  validation {
+    condition     = length(var.user-uuid) >= 3 && length(var.user-uuid) <= 63
     error_message = "The name of the S3 bucket must be between 3 and 63 characters long."
   }
 }
