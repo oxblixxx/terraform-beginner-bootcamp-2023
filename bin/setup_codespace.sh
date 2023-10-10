32d8e13bac8cb63b23337d246bcda57b7a214f22
Mutapha Pelumi Babatunde <97739416+oxblixxx@users.noreply.github.com>
2023-10-10 02:32:20 +0000

    #39 Add Terratowns server

--- bin/setup_codespace.sh
+++ bin/setup_codespace.sh
@@ -5,7 +5,7 @@ source '/workspaces/terraform-beginner-bootcamp-2023/bin/terraform'
 source '/workspaces/terraform-beginner-bootcamp-2023/bin/create_credentials_trfc'
 source '/workspace/terraform-beginner-bootcamp-2023/bin/tf_alias'
 source '/workspaces/terraform-beginner-bootcamp-2023/bin/tf_alias'
-cp '/workspace/terraform-beginner-bootcamp-2023/terraform.tfvars.example' '/workspace/terraform-beginner-bootcamp-2023/variables.tfvars'
+cp '/workspaces/terraform-beginner-bootcamp-2023/terraform.tfvars.example' '/workspaces/terraform-beginner-bootcamp-2023/variables.tfvars'
 source ~/.bash_profile
 nano /home/codespace/.terraform.d/credentials.tfrc.json
 
