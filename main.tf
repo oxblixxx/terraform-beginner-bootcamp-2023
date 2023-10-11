0276b3160a3fc0162b36954ff5fc4437da698400
Mutapha Pelumi Babatunde <97739416+oxblixxx@users.noreply.github.com>
2023-10-11 01:52:21 +0000

    #45 Configuring Terratowns provider

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+/*
 module "terrahouse_aws" {
   source               = "./modules/terrahouse_aws"
   user-uuid            = var.user-uuid
@@ -10,3 +11,4 @@ module "terrahouse_aws" {
   assets_path = var.assets_path
   # cdn_invalidate_path = var.cdn_invalidate_path
 }
+*/
